ba33f32e374f6a833adc09de8cd07dc1a9a02197
sahushubham <shubhamsahu2198@gmail.com>
2020-05-31 16:03:57 +0530

    Header and footer coursera

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React,{Component} from 'react';
 import logo from './logo.svg';
-import './App.css';
-
+import Menu from './components/MenuComponent';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import { DISHES } from './shared/dishes';
+import './App.css';
+import Main from './components/MainComponent';
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
+        <Main />
       </div>
     );
   }
