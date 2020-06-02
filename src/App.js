89c7bd12a745bab3bc118c743edca498715c9aa2
sahushubham <shubhamsahu2198@gmail.com>
2020-06-02 06:02:28 +0530

    Starting Point

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import Menu from './components/MenuComponent';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import { DISHES } from './shared/dishes';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
 import Main from './components/MainComponent';
 
 class App extends Component {
 
   render() {
     return (
+      <BrowserRouter>
       <div className="App">
         <Main />
       </div>
+    </BrowserRouter>
     );
   }
 }
