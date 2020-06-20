c170e6cc9eaa26c4a5e64f292286f3ad1f1e856d
rakaar <rka87338@gmail.com>
2020-06-21 05:24:04 +0530

    Corrected minor typo

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -30,7 +30,7 @@ function Home(props) {
                                                    {/* This is the second row */}
           <div id="row1" className="row">
             <div className="col-12 col-md-5 mx-auto">
-                <img width="100%" src={`${process.env.PUBLIC_URL}%assets/images/man.png`} alt="not found"/>
+                <img width="100%" src={`${process.env.PUBLIC_URL}assets/images/man.png`} alt="not found"/>
             </div>
             <div id="center-text" className="col-12 col-md-5 ">
               <div  >
