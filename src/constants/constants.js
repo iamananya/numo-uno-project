e112aff1d407c57f104cdbc8a8719eef7d0bcac7
Raghavendra Kaushik <44907757+rakaar@users.noreply.github.com>
2020-06-24 02:39:46 +0530

    Changed to HTTPS in blog URL

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,5 @@
 export const BACKEND_URL = "http://866a3e10cbce.ngrok.io";
 
-export const BLOG_URL = "http://blog.numouno.tech/ghost/api/v3/content";
+export const BLOG_URL = "https://blog.numouno.tech/ghost/api/v3/content";
 
 export const BLOG_KEY = "a6219f96224a2665310a44de9b";
