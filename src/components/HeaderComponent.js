b6e1047358b6246fe984c39bf7a1743f1f1c119e
shubham <shubhamsahu2198@gmail.com>
2020-07-03 13:17:22 +0530

    Modal implemented for login again page

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -3,7 +3,7 @@ import { Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem,
    Modal } from 'reactstrap';
 import {NavLink} from 'react-router-dom';
 import { BACKEND_URL } from '../constants/constants';
-
+import Login from './LoginModal';
 import axios from 'axios';
 
 
@@ -41,6 +41,7 @@ class Header extends Component {
         this.setState({
             isMdodalOpen: !this.state.isMdodalOpen
         });
+        
         document.getElementById("root").style.filter = "blur(20px)";
         if(this.state.isMdodalOpen){
             document.getElementById("root").style.filter = "blur(0px)";
@@ -149,7 +150,7 @@ class Header extends Component {
                             <NavLink className="nav-link"  to='/home'
                             style={{padding:"0px",
                                     margin:"0px"}}>
-                                <img src={`${process.env.PUBLIC_URL}/assets/images/logo.png`}
+                                <img src={`${process.env.PUBLIC_URL}/images/logo.webp`}
                                 height="50" width="auto" alt='Ristorante Con Fusion' />
                                 </NavLink>
                             </NavbarBrand>
@@ -170,7 +171,7 @@ class Header extends Component {
                             
                             <NavItem className="ml-5 ">
                                 <div onClick={this.toggleModal} id="login"> Login </div>
-                                
+                                <Login isMdodalOpen={this.state.isMdodalOpen}/>
                             </NavItem>
                             <NavItem className="ml-2 ">
                                 <button className="custom-button" onClick={this.toggleModalj}>
@@ -184,9 +185,9 @@ class Header extends Component {
                     </div>
                 </Navbar>
                 
-                <Modal  isOpen={this.state.isMdodalOpen} toggle={this.toggleModal} centered={true} 
+                {/* <Modal  isOpen={this.state.isMdodalOpen} toggle={this.toggleModal} centered={true} 
                 backdropTransition={{ timeout: 500000 }}
-                centered={true}  style={{borderRadius:"15px",border:"0px",background:"white"}} >
+                centered={true}  style={{letterSpacing:"1px"}} >
                     <div className="modal-content" style={{borderRadius:"15px"}}>
            <center>
                     <div className="container" style={{display:"flex",justifyContent:"center",flexDirection:"row",height:"auto",borderRadius:"15px"
@@ -196,30 +197,30 @@ class Header extends Component {
 	                <p style={{fontSize:"35px" , textAlign:"center" ,fontFamily:"Josefin Sans", color:"black"}}> Welcome Back</p>
                     <p style={{fontSize:"14px", fontFamily:"Montserrat",textAlign:"center",lineHeight:"18px",letterSpacing:"1px"}}>Signin to access personalized articles, podcasts, career enhancement services along with interest based professional communication groups.</p><br/>
 
- 	<center><a  href="./img/cd-logo.svg" className="fb btn" alt="logo missing">
+ 	<center><a  href="./img/cd-logo.svg" className="fb btn" alt="logo missing" style={{padding:"10px 12px"}}>
 		<div className="b">
-            	<p>.</p></div>
+            	<p style={{color:"white"}}>.</p></div>
 		<div className="f">
 		<img src={`${process.env.PUBLIC_URL}/assets/images/un.png`} style={{width: "40%",marginTop:"2px"}} alt="Logo missing"/></div>
-		<div className="e" style={{textAlign: "left",marginTop:"5px"}}>
+		<div className="e" style={{textAlign: "left",marginTop:"3px"}}>
 		Signin with Linkedin</div>
         	</a></center>
-       <center><a  href="http://google.com/" className="twitter btn">
+       <center><a  href="http://google.com/" className="twitter btn" style={{padding:"10px 12px"}}>
 		<div className="b">
-            	<p>.</p></div>
+            	<p style={{color:"white"}}>.</p></div>
 		<div className="f">
           	<img src={`${process.env.PUBLIC_URL}/assets/images/abc.svg`}  style={{width: "40%",marginTop:"2px"}} alt=" twitter logo missing"/></div>
-		<div className="e" style={{textAlign: "left",marginTop:"5px"}}>
+		<div className="e" style={{textAlign: "left",marginTop:"3px"}}>
  		Signin with Google</div>
         	</a></center>
 
-        <center><a  href="http://apple.com/" className="google btn">
+        <center><a  href="http://apple.com/" className="google btn" style={{padding:"10px 12px"}}>
 		<div className="b">
-            	<p>.</p></div>
+            	<p style={{color:"white"}}>.</p></div>
 		<div className="f">
-           	<img src={`${process.env.PUBLIC_URL}/assets/images/apple.webp`} style={{width: "40%",marginTop:"2px"} } alt=" twitter logo missing"
+           	<img src={`${process.env.PUBLIC_URL}/images/apple.webp`} style={{width: "40%",marginTop:"2px"} } alt=" twitter logo missing"
                /></div>
-		<div className="e" style={{textAlign: "left",marginTop:"5px"}}>
+		<div className="e" style={{textAlign: "left",marginTop:"3px"}}>
 		Signin with Apple</div>
         	</a></center>
 		<p  style={{margin:"1em 0"}}>
@@ -229,20 +230,21 @@ class Header extends Component {
 					<p style={{margin:"1em 0"}}>
 						<center>
                         <input className="cd-signin-modal__input cd-signin-modal__input--full-width cd-signin-modal__input--has-padding cd-signin-modal__input--has-border" id="signin-email" type="email" placeholder="E-mail" 
+                        onChange={e => this.setState({ loginEmail: e.target.value })}
                         style={{fontSize:"16px",width: "85%", fontFamily:"Josefin Sans",height:"80%"}}/>
 						<span className="cd-signin-modal__error">Error message here!</span></center>
 					</p>
 
-					<p style={{margin:"1em 0"}}>  
+					<p style={{margin:"1em 0 0.5em 0"}}>  
 						<center>
-						<input className="cd-signin-modal__input cd-signin-modal__input--full-width cd-signin-modal__input--has-padding cd-signin-modal__input--has-border" id="signin-password"   placeholder="Password"
+						<input className="cd-signin-modal__input cd-signin-modal__input--full-width cd-signin-modal__input--has-padding cd-signin-modal__input--has-border" id="signin-password" type="password" placeholder="Password"
                         onChange={e => this.setState({ loginPassword: e.target.value })}
                         style={{fontSize:"16px",width: "85%", fontFamily:"Josefin Sans",height:"80%"}}/>
 						
 						<span className="cd-signin-modal__error">Error message here!</span></center>
 					</p>
 					<div className="row">
-						<div className="b"><p>.</p></div>
+						<div className="b"><p style={{color:"white"}}>.</p></div>
 						<div className="c" style={{textAlign:"left"}}>
                             
 						<p  style={{margin:"0.5em 0 0.5em 0.3em"}}>
@@ -261,13 +263,14 @@ class Header extends Component {
 
 					<center><a href="google.com" style={{fontSize: "14px", fontFamily: "Montserrat"}}>Forgot Password?</a></center><br/>
 					
-					<center><p style={{fontSize:"14px" , fontFamily:"Montserrat"}}>No Account? <a href="#0" data-signin="signup" style={{fontSize: "14px", fontFamily: "Montserrat"}}>Create One</a></p></center>
+					<div onClick={this.toggleModalj} style={{fontSize:"12px" , fontFamily:"Montserrat"}}><center>No Account?
+                        Create One</center></div>
 
 				</form>
 				
 				</div></center>
                 </div>
-       </Modal>
+       </Modal> */}
        <Modal  isOpen={this.state.isMdodalOpenj} toggle={this.toggleModalj} centered={true} 
        backdropTransition={{ timeout: 500000 }} style={{letterSpacing:"1px"}}>
            <div className="modal-content" style={{borderRadius:"15px"}}>
@@ -282,29 +285,29 @@ class Header extends Component {
                     Create an account to access personalized articles, podcasts,
                      career enhancement services along with interest based professional communication groups.</p>
 
- 	<center><a  href="./img/cd-logo.svg" className="fb btn">
+ 	<center><a  href="./img/cd-logo.svg" className="fb btn" style={{padding:"10px 12px"}}>
 		<div className="b">
-            	<p>.</p></div>
+            	<p style={{color:"white"}}>.</p></div>
 		<div className="f">
 		<img src={`${process.env.PUBLIC_URL}/assets/images/un.png`} style={{width: "40%",marginTop:"2px"}} alt="Logo Missing"/></div>
-		<div className="e" style={{textAlign: "left",marginTop:"5px"}}>
+		<div className="e" style={{textAlign: "left",marginTop:"3px"}}>
 		Signin with Linkedin</div>
         	</a></center>
-       <center><a  href="http://google.com/" className="twitter btn">
+       <center><a  href="http://google.com/" className="twitter btn" style={{padding:"10px 12px"}}>
 		<div className="b">
-            	<p>.</p></div>
+            	<p style={{color:"white"}}>.</p></div>
 		<div className="f">
           	<img src={`${process.env.PUBLIC_URL}/assets/images/abc.svg`}  style={{width: "40%",marginTop:"2px"}} alt="Logo Missing"/></div>
-		<div className="e" style={{textAlign: "left" ,marginTop:"5px"}}>
+		<div className="e" style={{textAlign: "left" ,marginTop:"3px"}}>
  		Signin with Google</div>
         	</a></center>
 
-        <center><a  href="http://apple.com/" className="google btn">
+        <center><a  href="http://apple.com/" className="google btn" style={{padding:"10px 12px"}}>
 		<div className="b">
-            	<p>.</p></div>
+            	<p style={{color:"white"}}>.</p></div>
 		<div className="f">
            	<img src={`${process.env.PUBLIC_URL}/assets/images/apple.webp`} style={{width: "40%",marginTop:"2px"}} alt="Logo Missing"/></div>
-		<div className="e" style={{textAlign: "left",marginTop:"5px"}}>
+		<div className="e" style={{textAlign: "left",marginTop:"3px"}}>
 		Signin with Apple</div>
         	</a></center>
 		<p  style={{margin:".5em 0"}}>
@@ -313,7 +316,7 @@ class Header extends Component {
             
 
             <div class="row">
-					<div class="b"><p>.</p></div>
+					<div class="b"><p style={{color:"white"}}>.</p></div>
 					<div class="a">
 					
 					<p class="cd-signin-modal__fieldset">
@@ -322,7 +325,7 @@ class Header extends Component {
                          placeholder="First Name" style={{fontSize: "16px",width: "90%", fontFamily:"Josefin Sans",marginLeft:"5px",height:"70%"}} onChange={(e) => this.setState({firstName: e.target.value})}/>
 						
 					</p></div>
-					<div class="d"><p>.</p></div>
+					<div class="d"><p style={{color:"white"}}>.</p></div>
 					<div class="a">
 						<p class="cd-signin-modal__fieldset">
                         <input class="cd-signin-modal__input cd-signin-modal__input--full-width cd-signin-modal__input--has-padding cd-signin-modal__input--has-border" id="signup-userlname" type="text"
@@ -346,14 +349,14 @@ class Header extends Component {
 
 					<p style={{margin:"1em 0"}}>  
 						<center>
-						<input className="cd-signin-modal__input cd-signin-modal__input--full-width cd-signin-modal__input--has-padding cd-signin-modal__input--has-border" id="signin-password" type="text"  placeholder="Password"
+						<input className="cd-signin-modal__input cd-signin-modal__input--full-width cd-signin-modal__input--has-padding cd-signin-modal__input--has-border" id="signin-password" type="password"  placeholder="Password"
                         onChange={(e) => this.setState({password: e.target.value})}
                         style={{fontSize:"16px",width: "80%", fontFamily:"Josefin Sans",height:"70%"}}/>
 						
 						<span className="cd-signin-modal__error">Error message here!</span></center>
 					</p>
 				
-					<p style={{margin:"1em 0"}}>  
+					<p style={{margin:"1em 0 0.5em 0"}}>  
 						<center>
 						<input className="cd-signin-modal__input cd-signin-modal__input--full-width cd-signin-modal__input--has-padding cd-signin-modal__input--has-border" id="signin-password" type="password"  placeholder="Confirm Password"
                         onChange={(e) => this.setState({confirmPassword: e.target.value})}
@@ -363,9 +366,9 @@ class Header extends Component {
 					</p>
                     
 					<div className="row">
-						<div className="b"><p>.</p></div>
+						<div className="b"><p style={{color:"white"}}>.</p></div>
 						<div className="c" style={{width: "80%", textAlign:"left"}}>
-						<p  style={{margin:".5em 60px 0px 60px"}}>
+						<p  style={{margin:"0.5em 0 0.5em 0.3em"}}>
 							<input type="checkbox" id="remember-me" checked className="cd-signin-modal__input "/>
 							<label for="remember-me" style={{fontSize:"12px", fontFamily:"Montserrat"}}>I agree to Terms and Conditions</label>
 					</p></div></div>
@@ -379,8 +382,8 @@ class Header extends Component {
        							 </a></center>
 					</p>
 
-					<p style={{fontSize:"12px" , fontFamily:"Montserrat"}}><center>Already Have an Account?
-                        <a href="#0" data-signin="signup" style={{fontSize:"12px" , fontFamily:"Montserrat"}}> Signin</a></center></p>
+					<div onClick={this.toggleModal} style={{fontSize:"12px" , fontFamily:"Montserrat"}}><center>Already Have an Account?
+                        Signin</center></div>
 
 				</form>
 				
