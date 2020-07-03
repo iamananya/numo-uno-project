747057402ef96ab014dff3a5a45080885a044a18
Soumya Tarafder <63846042+Soumya624@users.noreply.github.com>
2020-07-04 00:39:02 +0530

    Update HeaderComponent.js

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -76,7 +76,7 @@ class Header extends Component {
           this.state.touched.email &&
           email.split("").filter((x) => x === "@").length !== 1
         )
-          errors.email = "Email should contain a @ sign";
+          errors.email = "Email is not correct";
     
         return errors;
       }
@@ -415,9 +415,17 @@ class Header extends Component {
 						<center>
                         <FormGroup>
                         <Input className="cd-signin-modal__Input cd-signin-modal__input--full-width cd-signin-modal__input--has-padding cd-signin-modal__input--has-border" id="email" type="email" placeholder="E-mail" 
-                        onChange={(e) => this.setState({email: e.target.value})}
+                        name="email"
+			value={this.state.email} 
+			valid={errors.email === ""&&this.state.touched.email}
+			invalid={errors.email!== ""}
+			onBlur={this.handleBlur("email")}
+			onChange={this.handleInputChange}
+			onChange={(e) => this.setState({email: e.target.value})}
                         style={{fontSize:"16px",width: "80%", fontFamily:"Josefin Sans" ,height:"70%"}}/>
-						<span className="cd-signin-modal__error">Error message here!</span>
+			<FormFeedback style={{ fontSize: "14px" }}>
+                            {errors.email}
+                            </FormFeedback>			
                         </FormGroup>
                         </center>
 					</p>
