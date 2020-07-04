a66fbcec0976fd49b702d3b632f0fa4e2d1100e3
Soumya Tarafder <63846042+Soumya624@users.noreply.github.com>
2020-07-04 14:32:27 +0530

    Update HeaderComponent.js

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -77,7 +77,7 @@ class Header extends Component {
         if (this.state.touched.email && email.split("").filter((x) => x === "@").length !== 1)
           errors.email = "Email is not correct";
     
-      if (this.state.touched.loginEmail && loginEmail.split("").filter((x) => x === "@").length !== 1)
+      if (this.state.touched.loginEmail && email.split("").filter((x) => x === "@").length !== 1)
         errors.loginEmail = "Email is not correct";
 
         return errors;
