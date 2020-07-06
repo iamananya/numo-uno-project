c169bee7bcf20f736836fd9093cc51254a1ac2f9
Soumya Tarafder <63846042+Soumya624@users.noreply.github.com>
2020-07-06 19:03:06 +0530

    Update SignupComponent.js

--- src/components/SignupComponent.js
+++ src/components/SignupComponent.js
@@ -247,7 +247,7 @@ class Signup extends Component {
 						<p  style={{margin:"0.5em 0 0.5em 0.3em"}}>
                             <FormGroup>
 							<Input type="checkbox" id="remember-me" checked className="cd-signin-modal__input "/>
-							<label for="remember-me" style={{fontSize:"12px", fontFamily:"Montserrat"}}>I agree to Terms and Conditions</label>
+							<label for="remember-me" style={{fontSize:"13px", fontFamily:"Montserrat"}}> I agree to Terms and Conditions</label>
                             </FormGroup>
                     </p></div></div>
 
